8abb054076d787f1396affe9596f860f48f38f1e
andreaheas <andrea.heas@gmail.com>
2021-07-02 12:32:47 -0500

    Prueba

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+/*Le hacemos saber que estamos dando click*/
+let boton = document.querySelector ("button");
+console.log (boton)
+
+let span = document.getElementById("validacion");
+let numeroTarjeta = document.getElementById("numeroTarjeta");
+
+boton.addEventListener("click");
+
+numeroTarjeta.addEventListener ("keyup"),(Event) =>{ 
+
+    
+}
